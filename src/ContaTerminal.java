b9b8a9739b8002cc3708ab80392382fb9424499c
priscilacastro <priscilac95@gmail.com>
2024-06-06 23:08:27 +0100

    ContaBanco

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -1,5 +1,19 @@
 public class ContaTerminal {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        int Saldo = 25;
+        int ValorSolicitado = 18;
+            if (ValorSolicitado < Saldo){
+                System.out.println("O saldo atual é: 7 euros" );
+            }
+            else if (ValorSolicitado > Saldo){
+                System.out.println("Saldo Insuficiente");
+
+            }
+        
     }
 }
+
+
+
+
+
